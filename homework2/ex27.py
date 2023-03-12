43779dc3907ea5dec9516649fde125f134fcc8c4
krjoanna <127695263+krjoanna@users.noreply.github.com>
2023-03-12 22:24:10 +0100

    Update ex27.py

--- homework2/ex27.py
+++ homework2/ex27.py
@@ -7,22 +7,4 @@ RomArabDict["C"]= 100
 RomArabDict["D"]= 500
 RomArabDict["M"]= 1000
 
-print(RomArabDict[0])
-print("Write roman number: ") 
-RomNumb = input()
-
-ArabNumb = 0
-i = 0
-for i in range(1,7):
-    ArabNumb = RomArabDict[i]
-    if RomArabDict[i] > RomArabDict[i - 1]:
-        ArabNumb += RomArabDict[i] - 2 * RomArabDict[i - 1]
-    else:
-        ArabNumb += RomArabDict[i]
-    print(ArabNumb)
-
-    j = RomArabDict[i]
-    if j < ArabNumb:
-        ArabNumb = ArabNumb + RomArabDict[i]
-
-print(RomNumb, "converted to arabic numbers is: ", ArabNumb)
+print(RomArabDict)
