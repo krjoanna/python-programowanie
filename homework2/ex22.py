ba420e668e498102cea456a6cfac7da25945a065
krjoanna <127695263+krjoanna@users.noreply.github.com>
2023-03-13 00:10:27 +0100

    Update ex22.py

--- homework2/ex22.py
+++ homework2/ex22.py
@@ -1,12 +1,10 @@
 #bool
 ## Explain the results.
 x = 5
-x == 5 and 5         # 3    --> zwraca 3 (ostatni int), bo x jest równe 5
+x == 5 and 3       # 3    --> zwraca 3 (ostatni int), bo x jest równe 5
 x == 4 and 3                # False --> zwraca False, bo x nie jest równe 4
 3 and x == 5                  # True  --> zwraca True, bo x jest równy 5
 3 and x == 4                  # False --> zwraca False, bo x nie jest równy 4
 
 isinstance(True, int)         # True --> bo kiedyś true== 1 false == 0 , czyli są int (należy do podklasy int)
 isinstance(True, bool)        # True --> True należy do klasy bool
-
-
